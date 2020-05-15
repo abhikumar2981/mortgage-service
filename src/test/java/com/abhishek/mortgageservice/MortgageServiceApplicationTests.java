bf20e7c9eed99461ff510f695fcacedf7dd3b919
Abhishek Kumar <abhi.kumar2981@gmail.com>
2020-05-15 13:01:00 +0530

    Initial Commit with basic spring rest api project

---
+++ src/test/java/com/abhishek/mortgageservice/MortgageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishek.mortgageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MortgageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
